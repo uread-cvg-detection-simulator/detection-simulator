9862421bab7b8baeef15178497799169c0ab4702
Jonathan Boyle <j.n.boyle@reading.ac.uk>
2023-07-26 12:59:28 +0100

    feat: add colour change on the .colour parameter of the agent

---
+++ detection-simulator/agents/agent.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+@export var colour: Color = Color.WHITE : set = _set_sprite_colour
+@onready var sprite_to_colour_change: Sprite2D = $sprite
+
+var initialised: bool = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	initialised = true
+
+	if colour != Color.WHITE:
+		_set_sprite_colour(colour)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _set_sprite_colour(new_colour: Color):
+	colour = new_colour
+
+	if initialised:
+		sprite_to_colour_change.material.set_shader_parameter("new_colour", Vector4(new_colour.r, new_colour.g, new_colour.b, new_colour.a))
+
