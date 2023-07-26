0c1c2ced523fb412151461523247082af57f5b40
Jonathan Boyle <j.n.boyle@reading.ac.uk>
2023-07-26 11:01:45 +0100

    fix(nix): GODOT_CMD not working in nonnix shell

--- flake.nix
+++ flake.nix
@@ -32,7 +32,7 @@
 					default = pkgs.mkShell {
 						packages = dev-package-list ++ [ nix-shell-script ];
 						shellHook = ''
-							export GODOT_CMD=${pkgs.godot_4}/bin/godot4
+							export GODOT_CMD="${pkgs.godot_4}/bin/godot4"
 							set -a; source .env; set +a;
 						'';
 					};
@@ -40,7 +40,7 @@
 					nonnix = pkgs.mkShell {
 						packages = dev-package-list ++ [ pkgs.nixgl.auto.nixGLDefault nix-shell-script ];
 						shellHook = ''
-							export GODOT_CMD=nixGL ${pkgs.godot_4}/bin/godot4
+							export GODOT_CMD="nixGL ${pkgs.godot_4}/bin/godot4"
 							set -a; source .env; set +a;
 						'';
 					};
