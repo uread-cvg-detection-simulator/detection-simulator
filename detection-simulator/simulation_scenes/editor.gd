f47106f602b51a0bb1cbb89813b32f90a3901366
Jonathan Boyle <programmerfalcon@gmail.com>
2023-07-27 12:04:18 +0100

    feat: add right click option to return to centre

--- detection-simulator/simulation_scenes/editor.gd
+++ detection-simulator/simulation_scenes/editor.gd
@@ -10,12 +10,15 @@ var _scrolling = false ## Set when drag-scrolling
 var _right_click_position = null ## Set to ensure popup menus act on correct mouse position
 
 enum empty_menu_enum {
-	SPAWN_AGENT
+	SPAWN_AGENT,
+	RETURN_TO_CENTRE
 }
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	_rightclick_empty.add_item("Spawn New Agent", empty_menu_enum.SPAWN_AGENT)
+	_rightclick_empty.add_separator()
+	_rightclick_empty.add_item("Centre Grid", empty_menu_enum.RETURN_TO_CENTRE)
 	_rightclick_empty.connect("id_pressed", self._on_empty_menu_press)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -37,7 +40,7 @@ func _input(event):
 		if event.button_index == MOUSE_BUTTON_RIGHT and event.is_pressed():
 			# Handle Menu
 			_right_click(event)
-		
+
 
 	if event is InputEventMouseMotion and _scrolling:
 		var tmp_event: InputEventMouseMotion = event
@@ -52,10 +55,15 @@ func _right_click(event: InputEventMouseButton):
 
 	_rightclick_empty.popup(Rect2i(mouse_rel_pos.x + window_size.x, mouse_rel_pos.y + window_size.y, _rightclick_empty.size.x, _rightclick_empty.size.y))
 	_right_click_position = mouse_pos
+	
+	print_debug("Right click at (%.2f, %.2f)" % [float(mouse_pos.x) / 64, - float(mouse_pos.y) / 64])
 
 func _on_empty_menu_press(id: int):
-	if id == empty_menu_enum.SPAWN_AGENT:
-		spawn_agent(_right_click_position)
+	match id:
+		empty_menu_enum.SPAWN_AGENT:
+			spawn_agent(_right_click_position)
+		empty_menu_enum.RETURN_TO_CENTRE:
+			$Camera2D.set_global_position(Vector2(0, 0))
 
 func spawn_agent(position: Vector2):
 	var new_agent: Agent = _agent_base.instantiate()
