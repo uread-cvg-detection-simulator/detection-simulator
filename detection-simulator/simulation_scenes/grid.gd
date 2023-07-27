d4d5478178445acc6d9e354a5f0e5f622804358a
Jonathan Boyle <programmerfalcon@gmail.com>
2023-07-27 03:28:43 +0100

    feat: add thick grid lines every 10

--- detection-simulator/simulation_scenes/grid.gd
+++ detection-simulator/simulation_scenes/grid.gd
@@ -10,15 +10,15 @@ var camera_zoom = null
 
 func _draw():
 	if on:
-		var size = get_viewport_rect().size * camera.zoom / 2
+		var size = camera.get_viewport_rect().size * camera.zoom / 2
 
 		var camera_position = camera.position
 
 		for i in range(int((camera_position.x - size.x) / grid_size) - 1, int((size.x + camera_position.x) / grid_size) + 1):
-			draw_line(Vector2(i * grid_size, camera_position.y + size.y + 100), Vector2(i * grid_size, camera_position.y - size.y - 100), grid_colour)
+			draw_line(Vector2(i * grid_size, camera_position.y + size.y + 100), Vector2(i * grid_size, camera_position.y - size.y - 100), grid_colour, 3.0 if (i % 10 == 0) else 1.0)
 
 		for i in range(int((camera_position.y - size.y) / grid_size) - 1, int((size.y + camera_position.y) / grid_size) + 1):
-			draw_line(Vector2(camera_position.x + size.x + 100, i * grid_size), Vector2(camera_position.x - size.x - 100, i * grid_size), grid_colour)
+			draw_line(Vector2(camera_position.x + size.x + 100, i * grid_size), Vector2(camera_position.x - size.x - 100, i * grid_size), grid_colour, 3.0 if (i % 10 == 0) else 1.0)
 
 func _ready():
 	camera.connect("camera_moved", self._on_camera_change)
