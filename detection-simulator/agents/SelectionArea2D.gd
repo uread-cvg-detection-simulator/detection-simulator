45aa84c57844c8e6210ba03115e0b73fcbae2922
Jonathan Boyle <programmerfalcon@gmail.com>
2023-08-01 20:49:37 +0100

    feat: add passthrough only mode for SelectionArea2D

--- detection-simulator/agents/SelectionArea2D.gd
+++ detection-simulator/agents/SelectionArea2D.gd
@@ -4,9 +4,13 @@ extends Area2D
 # Based on https://www.youtube.com/watch?v=K9DenBHIDzU
 
 signal selection_toggled(selection)
+signal mouse_click(button, event)
 
-@export var exclusive = true
+@export var selection_exclusive = true
 @export var selection_action = "mouse_selected"
+@export var multi_selection_action = "mouse_multi_select"
+
+@export var passthrough_mode = false ## Passes through left click rather than select
 
 var selected: bool = false : set = _set_selected
 
@@ -21,12 +25,26 @@ func _set_selected(new_selected: bool):
 	emit_signal("selection_toggled", selected)
 
 func _make_exclusive():
-	if exclusive:
+	if selection_exclusive:
 		get_tree().call_group("selected", "_set_selected", false)
 
 # Must be connected to Area's input_event
-func _on_input_event(viewport: Viewport, event, shape_idx):
-	if event.is_action_pressed(selection_action):
-		selected = not selected
+func _input_event(viewport: Viewport, event, shape_idx):
+	if event.is_action_pressed(multi_selection_action):
+		if !passthrough_mode:
+			selection_exclusive = false
+			selected = not selected
+			selection_exclusive = true
+		
 		viewport.set_input_as_handled()
+	elif event.is_action_pressed(selection_action):
+		if !passthrough_mode:
+			selected = not selected
+		
+		emit_signal("mouse_click", MOUSE_BUTTON_LEFT, event)
+		viewport.set_input_as_handled()
+	elif event.is_action_pressed("mouse_menu"):
+		emit_signal("mouse_click", MOUSE_BUTTON_RIGHT, event)
+		viewport.set_input_as_handled()
+
 
