be8031a95de06ff605ebe269e56456c3fe11405d
Jonathan Boyle <j.n.boyle@reading.ac.uk>
2023-11-22 11:18:25 +0000

    feat(agent): speed up check for waypoint state if not needing to wait

--- detection-simulator/agents/agent_states/wait_waypoint_conditions.gd
+++ detection-simulator/agents/agent_states/wait_waypoint_conditions.gd
@@ -15,11 +15,14 @@ func update(_delta: float) -> void:
 
 # Virtual function. Corresponds to the `_physics_process()` callback.
 func physics_update(_delta: float) -> void:
+	if _check_ready():
+		state_machine.transition_to("follow_waypoints")
+
+func _check_ready() -> bool:
 	if playing_next_move_time < PlayTimer.current_time:
-		
 		var ready = true
 		var max_wait_time = 0
-		
+
 		playing_last_waypoint.linked_ready = true
 
 		# Wait until all linked nodes are ready
@@ -40,26 +43,30 @@ func physics_update(_delta: float) -> void:
 						ready = false
 					else:
 						continue
-		
+
 		if max_wait_time != 0 and not ready:
 			var diff = (max_wait_time - (playing_last_waypoint.param_wait_time if playing_last_waypoint.param_wait_time else 0))
 			playing_next_move_time = PlayTimer.current_time + diff
-		
-		if ready:
-			state_machine.transition_to("follow_waypoints")
+
+		return ready
+	else:
+		return false
 
 # Virtual function. Called by the state machine upon changing the active state. The `msg` parameter
 # is a dictionary with arbitrary data the state can use to initialize itself.
 func enter(msg := {}, _old_state_name: String = "") -> bool:
 	if "playing_last_waypoint" in msg:
 		playing_last_waypoint = msg["playing_last_waypoint"]
-	
+
 	if "playing_waypoint" in msg:
 		playing_waypoint = msg["playing_waypoint"]
-		
+
 	if "playing_last_waypoint" in msg:
 		playing_next_move_time = msg["playing_next_move_time"]
-		
+
+	if _check_ready():
+		return false
+
 	return true
 
 # Virtual function. Called by the state machine before changing the active state. Use this function
