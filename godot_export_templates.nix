c96bc19a0d5edadb75c38b4b7544a4c4289b19ba
Jonathan Boyle <programmerfalcon@gmail.com>
2023-11-27 11:17:49 +0000

    chore(flake): update for godot 4.1.3

--- godot_export_templates.nix
+++ godot_export_templates.nix
@@ -2,13 +2,17 @@
 let
 	godot_export_hashes = version: {
 		"4.1.1" = "sha256-22BPoqPmcako7EmBmyJAet9chSKQZ4RXUktendyK2fU=";
+		"4.1.3" = "sha256-QZBa0/8jLTf1XHHsFZnYM5v+81P2GN5obev4fT0OtHM=";
 	}.${version} or (
 		pkgs.lib.warn "No hash for Godot version ${version} found" pkgs.lib.fakeHash
 	);
 
+	# If 'stable' in godot_version, remove it
+	godot_version_mod = pkgs.lib.replaceStrings ["-stable"] [""] godot_version;
+
 	godot_export_templates_src = pkgs.fetchurl {
-		url = "https://github.com/godotengine/godot/releases/download/${godot_version}-stable/Godot_v${godot_version}-stable_export_templates.tpz";
-		hash = godot_export_hashes godot_version;
+		url = "https://github.com/godotengine/godot/releases/download/${godot_version_mod}-stable/Godot_v${godot_version_mod}-stable_export_templates.tpz";
+		hash = godot_export_hashes godot_version_mod;
 	};
 
 
