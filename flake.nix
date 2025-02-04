525b4106f01e1d627b0ff1016aa49d007b489ef6
Jonathan Boyle <j.n.boyle@reading.ac.uk>
2025-02-04 14:30:43 +0000

    chore: update to godot 4.3

--- flake.nix
+++ flake.nix
@@ -1,127 +1,150 @@
 {
-	inputs = {
-		nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-		nixpkgs-godot.url = "github:NixOS/nixpkgs/666fc80e7b2afb570462423cb0e1cf1a3a34fedd";
-		flake-utils.url = "github:numtide/flake-utils";
-		nixGL = {
-			url = "github:guibou/nixGL";
-			inputs.nixpkgs.follows = "nixpkgs";
-		};
-	};
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs-godot.url = "github:NixOS/nixpkgs/9cf669b81143caf2cad0664c6610086ae0b84184";
+    flake-utils.url = "github:numtide/flake-utils";
+    nixGL = {
+      url = "github:guibou/nixGL";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 
-	outputs = { nixpkgs, nixpkgs-godot, flake-utils, nixGL, ... } :
-		flake-utils.lib.eachDefaultSystem (system:
-			let
-				pkgs = import nixpkgs {
-					system = system;
-					config = { allowUnfree = true; };
-					overlays = [ nixGL.overlay ];
-				};
+  outputs =
+    {
+      nixpkgs,
+      nixpkgs-godot,
+      flake-utils,
+      nixGL,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          system = system;
+          config = {
+            allowUnfree = true;
+          };
+          overlays = [ nixGL.overlay ];
+        };
 
-				pkgs-godot = import nixpkgs-godot {
-					system = system;
-					config = { allowUnfree = true; };
-					overlays = [ nixGL.overlay ];
-				};
+        pkgs-godot = import nixpkgs-godot {
+          system = system;
+          config = {
+            allowUnfree = true;
+          };
+          overlays = [ nixGL.overlay ];
+        };
 
-				lib = pkgs.lib;
+        lib = pkgs.lib;
 
-				dev-package-list = with pkgs-godot; [ godot_4 ] ++ (with pkgs; [gdtoolkit_4]);
+        dev-package-list = with pkgs-godot; [ godot_4 ] ++ (with pkgs; [ gdtoolkit_4 ]);
 
-				godot_export_templates = import ./godot_export_templates.nix { pkgs = pkgs-godot; godot_version = pkgs-godot.godot_4.version; };
+        godot_export_templates = import ./godot_export_templates.nix {
+          pkgs = pkgs-godot;
+          godot_version = pkgs-godot.godot_4.version;
+        };
 
-				nix-shell-script = pkgs.writeShellScriptBin "godot" ''
-					#!/bin/bash
-					$GODOT_BIN --path $PROJECT_NAME -e
-				'';
+        nix-shell-script = pkgs.writeShellScriptBin "godot" ''
+          					#!/bin/bash
+          					$GODOT_BIN --path $PROJECT_NAME -e
+          				'';
 
-				godot-build-windows = pkgs.writeShellScriptBin "godot-build-windows" ''
-					#!/bin/bash
-					BUILD_PATH=build/windows
-					mkdir -p $BUILD_PATH
-					$GODOT_BIN --path $PROJECT_NAME --headless --export-release "Windows Desktop" ../$BUILD_PATH/$PROJECT_NAME.exe
-				'';
+        godot-build-windows = pkgs.writeShellScriptBin "godot-build-windows" ''
+          					#!/bin/bash
+          					BUILD_PATH=build/windows
+          					mkdir -p $BUILD_PATH
+          					$GODOT_BIN --path $PROJECT_NAME --headless --export-release "Windows Desktop" ../$BUILD_PATH/$PROJECT_NAME.exe
+          				'';
 
-				godot-build-linux = pkgs.writeShellScriptBin "godot-build-linux" ''
-					#!/bin/bash
-					BUILD_PATH=build/linux
-					mkdir -p $BUILD_PATH
-					$GODOT_BIN --path $PROJECT_NAME --headless --export-release "Linux/X11" ../$BUILD_PATH/$PROJECT_NAME.x86_64
-				'';
+        godot-build-linux = pkgs.writeShellScriptBin "godot-build-linux" ''
+          					#!/bin/bash
+          					BUILD_PATH=build/linux
+          					mkdir -p $BUILD_PATH
+          					$GODOT_BIN --path $PROJECT_NAME --headless --export-release "Linux/X11" ../$BUILD_PATH/$PROJECT_NAME.x86_64
+          				'';
 
-				godot-build-macos = pkgs.writeShellScriptBin "godot-build-macos" ''
-					#!/bin/bash
-					BUILD_PATH=build/macos
-					mkdir -p $BUILD_PATH
-					$GODOT_BIN --path $PROJECT_NAME --headless --export-release "macOS" ../$BUILD_PATH/$PROJECT_NAME.zip
-				'';
+        godot-build-macos = pkgs.writeShellScriptBin "godot-build-macos" ''
+          					#!/bin/bash
+          					BUILD_PATH=build/macos
+          					mkdir -p $BUILD_PATH
+          					$GODOT_BIN --path $PROJECT_NAME --headless --export-release "macOS" ../$BUILD_PATH/$PROJECT_NAME.zip
+          				'';
 
-				godot-build-html5 = pkgs.writeShellScriptBin "godot-build-html5" ''
-					#!/bin/bash
-					BUILD_PATH=build/html5
-					mkdir -p $BUILD_PATH
-					$GODOT_BIN --path $PROJECT_NAME --headless --export-release "Web" ../$BUILD_PATH/index.html
-				'';
+        godot-build-html5 = pkgs.writeShellScriptBin "godot-build-html5" ''
+          					#!/bin/bash
+          					BUILD_PATH=build/html5
+          					mkdir -p $BUILD_PATH
+          					$GODOT_BIN --path $PROJECT_NAME --headless --export-release "Web" ../$BUILD_PATH/index.html
+          				'';
 
-				godot-build-all = pkgs.writeShellScriptBin "godot-build-all" ''
-					#!/bin/bash
-					${godot-build-windows}/bin/godot-build-windows
-					${godot-build-linux}/bin/godot-build-linux
-					${godot-build-macos}/bin/godot-build-macos
-					${godot-build-html5}/bin/godot-build-html5
-				'';
+        godot-build-all = pkgs.writeShellScriptBin "godot-build-all" ''
+          					#!/bin/bash
+          					${godot-build-windows}/bin/godot-build-windows
+          					${godot-build-linux}/bin/godot-build-linux
+          					${godot-build-macos}/bin/godot-build-macos
+          					${godot-build-html5}/bin/godot-build-html5
+          				'';
 
-				godot-run-tests = pkgs.writeShellScriptBin "godot-run-tests" ''
-					#!/bin/bash
-					cd $PROJECT_NAME
-					./addons/gdUnit4/runtest.sh -a test -c
-				'';
+        godot-run-tests = pkgs.writeShellScriptBin "godot-run-tests" ''
+          					#!/bin/bash
+          					cd $PROJECT_NAME
+          					./addons/gdUnit4/runtest.sh -a test -c
+          				'';
 
-				all-scripts = [ nix-shell-script godot-build-windows godot-build-linux godot-build-macos godot-build-html5 godot-build-all godot-run-tests];
+        all-scripts = [
+          nix-shell-script
+          godot-build-windows
+          godot-build-linux
+          godot-build-macos
+          godot-build-html5
+          godot-build-all
+          godot-run-tests
+        ];
 
-				project_name = "detection-simulator";
-			in
-			{
-				devShells = {
-					default = pkgs.mkShell {
-						packages = dev-package-list ++ all-scripts ++ [ godot_export_templates ];
-						GODOT_RAW_BIN = "${pkgs-godot.godot_4}/bin/godot4";
-						GODOT_BIN = "${pkgs-godot.godot_4}/bin/godot4";
+        project_name = "detection-simulator";
+      in
+      {
+        devShells = {
+          default = pkgs.mkShell {
+            packages = dev-package-list ++ all-scripts ++ [ godot_export_templates ];
+            GODOT_RAW_BIN = "${pkgs-godot.godot_4}/bin/godot4";
+            GODOT_BIN = "${pkgs-godot.godot_4}/bin/godot4";
 
-						shellHook = ''
-							set -a; source .env; set +a;
+            shellHook = ''
+              							set -a; source .env; set +a;
 
-							# Link export templates if not already done ~/.local/share/godot/export_templates/VERSION.stable (update if symlink is to incorrect location)
-							if [ ! -d ~/.local/share/godot/export_templates/${godot_export_templates.version}.stable ]; then
-								mkdir -p ~/.local/share/godot/export_templates
-								ln -s ${godot_export_templates}/templates ~/.local/share/godot/export_templates/${godot_export_templates.version}.stable
-							elif [ "$(readlink ~/.local/share/godot/export_templates/${godot_export_templates.version}.stable)" != "${godot_export_templates}/templates" ]; then
-								rm -r ~/.local/share/godot/export_templates/${godot_export_templates.version}.stable
-								ln -s ${godot_export_templates}/templates ~/.local/share/godot/export_templates/${godot_export_templates.version}.stable
-							fi
-						'';
-					};
+              							# Link export templates if not already done ~/.local/share/godot/export_templates/VERSION.stable (update if symlink is to incorrect location)
+              							if [ ! -d ~/.local/share/godot/export_templates/${godot_export_templates.version}.stable ]; then
+              								mkdir -p ~/.local/share/godot/export_templates
+              								ln -s ${godot_export_templates}/templates ~/.local/share/godot/export_templates/${godot_export_templates.version}.stable
+              							elif [ "$(readlink ~/.local/share/godot/export_templates/${godot_export_templates.version}.stable)" != "${godot_export_templates}/templates" ]; then
+              								rm -r ~/.local/share/godot/export_templates/${godot_export_templates.version}.stable
+              								ln -s ${godot_export_templates}/templates ~/.local/share/godot/export_templates/${godot_export_templates.version}.stable
+              							fi
+              						'';
+          };
 
-					nonnix = pkgs.mkShell {
-						packages = dev-package-list ++ [ pkgs.nixgl.auto.nixGLDefault ] ++ all-scripts;
-						GODOT_RAW_BIN = "${pkgs-godot.godot_4}/bin/godot4";
-						GODOT_BIN = "nixGL ${pkgs-godot.godot_4}/bin/godot4";
+          nonnix = pkgs.mkShell {
+            packages = dev-package-list ++ [ pkgs.nixgl.auto.nixGLDefault ] ++ all-scripts;
+            GODOT_RAW_BIN = "${pkgs-godot.godot_4}/bin/godot4";
+            GODOT_BIN = "nixGL ${pkgs-godot.godot_4}/bin/godot4";
 
-						shellHook = ''
-							set -a; source .env; set +a;
+            shellHook = ''
+              							set -a; source .env; set +a;
 
-							# Link export templates if not already done ~/.local/share/godot/export_templates/VERSION.stable (update if symlink is to incorrect location)
-							if [ ! -d ~/.local/share/godot/export_templates/${godot_export_templates.version}.stable ]; then
-								mkdir -p ~/.local/share/godot/export_templates
-								ln -s ${godot_export_templates}/templates ~/.local/share/godot/export_templates/${godot_export_templates.version}.stable
-							elif [ "$(readlink ~/.local/share/godot/export_templates/${godot_export_templates.version}.stable)" != "${godot_export_templates}/templates" ]; then
-								rm -r ~/.local/share/godot/export_templates/${godot_export_templates.version}.stable
-								ln -s ${godot_export_templates}/templates ~/.local/share/godot/export_templates/${godot_export_templates.version}.stable
-							fi
-						'';
-					};
-				};
-			}
-		);
+              							# Link export templates if not already done ~/.local/share/godot/export_templates/VERSION.stable (update if symlink is to incorrect location)
+              							if [ ! -d ~/.local/share/godot/export_templates/${godot_export_templates.version}.stable ]; then
+              								mkdir -p ~/.local/share/godot/export_templates
+              								ln -s ${godot_export_templates}/templates ~/.local/share/godot/export_templates/${godot_export_templates.version}.stable
+              							elif [ "$(readlink ~/.local/share/godot/export_templates/${godot_export_templates.version}.stable)" != "${godot_export_templates}/templates" ]; then
+              								rm -r ~/.local/share/godot/export_templates/${godot_export_templates.version}.stable
+              								ln -s ${godot_export_templates}/templates ~/.local/share/godot/export_templates/${godot_export_templates.version}.stable
+              							fi
+              						'';
+          };
+        };
+      }
+    );
 
 }
--- godot_export_templates.nix
+++ godot_export_templates.nix
@@ -1,40 +1,44 @@
 { pkgs, godot_version }:
 let
-	godot_export_hashes = version: {
-		"4.1.1" = "sha256-22BPoqPmcako7EmBmyJAet9chSKQZ4RXUktendyK2fU=";
-		"4.1.3" = "sha256-QZBa0/8jLTf1XHHsFZnYM5v+81P2GN5obev4fT0OtHM=";
-		"4.2" = "sha256-LOH/LScK6ghWoaqhiFyc5st8YHQqO8M0MW4yt5ByGY4=";
-	}.${version} or (
-		pkgs.lib.warn "No hash for Godot version ${version} found" pkgs.lib.fakeHash
-	);
+  godot_export_hashes =
+    version:
+    {
+      "4.1.1" = "sha256-22BPoqPmcako7EmBmyJAet9chSKQZ4RXUktendyK2fU=";
+      "4.1.3" = "sha256-QZBa0/8jLTf1XHHsFZnYM5v+81P2GN5obev4fT0OtHM=";
+      "4.2" = "sha256-LOH/LScK6ghWoaqhiFyc5st8YHQqO8M0MW4yt5ByGY4=";
+      "4.3" = "sha256-9fENuvVqeQg0nmS5TqjCyTwswR+xAUyVZbaKK7Q3uSI=";
+    }
+    .${version} or (pkgs.lib.warn "No hash for Godot version ${version} found" pkgs.lib.fakeHash);
 
-	# If 'stable' in godot_version, remove it
-	godot_version_mod_no_stable = pkgs.lib.replaceStrings ["-stable"] [""] godot_version;
+  # If 'stable' in godot_version, remove it
+  godot_version_mod_no_stable = pkgs.lib.replaceStrings [ "-stable" ] [ "" ] godot_version;
 
-	godot_version_special_cases = version: {
-		"4.2.0" = "4.2";
-	}.${version} or version;
+  godot_version_special_cases =
+    version:
+    {
+      "4.2.0" = "4.2";
+    }
+    .${version} or version;
 
-	godot_version_mod = godot_version_special_cases godot_version_mod_no_stable;
+  godot_version_mod = godot_version_special_cases godot_version_mod_no_stable;
 
-	godot_export_templates_src = pkgs.fetchurl {
-		url = "https://github.com/godotengine/godot/releases/download/${godot_version_mod}-stable/Godot_v${godot_version_mod}-stable_export_templates.tpz";
-		hash = godot_export_hashes godot_version_mod;
-	};
+  godot_export_templates_src = pkgs.fetchurl {
+    url = "https://github.com/godotengine/godot/releases/download/${godot_version_mod}-stable/Godot_v${godot_version_mod}-stable_export_templates.tpz";
+    hash = godot_export_hashes godot_version_mod;
+  };
 
+  godot_export_templates = pkgs.stdenv.mkDerivation {
+    name = "godot_export_templates-${godot_version}";
+    src = godot_export_templates_src;
+    version = godot_version_mod;
+    phases = [ "unpackPhase" ];
 
-	godot_export_templates = pkgs.stdenv.mkDerivation {
-		name = "godot_export_templates-${godot_version}";
-		src = godot_export_templates_src;
-		version = godot_version_mod;
-		phases = [ "unpackPhase" ];
+    buildInputs = [ pkgs.unzip ];
 
-		buildInputs = [ pkgs.unzip ];
-
-		unpackPhase = ''
-			mkdir -p $out
-			unzip -d $out $src
-		'';
-	};
+    unpackPhase = ''
+      			mkdir -p $out
+      			unzip -d $out $src
+      		'';
+  };
 in
 godot_export_templates
